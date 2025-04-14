99f6869c62033c2187d2ce0050bbfe0e3c56a8db
daniheyl <35920095+daniheyl@users.noreply.github.com>
2025-04-14 15:06:54 +0200

    added statuses on commit

--- tasks/apps.py
+++ tasks/apps.py
@@ -4,3 +4,12 @@ from django.apps import AppConfig
 class TasksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'tasks'
+
+    def ready(self):
+        from .models import Status
+        if not Status.objects.exists():
+            Status.objects.bulk_create([
+                Status(name='Pending', description='Task is pending'),
+                Status(name='In Progress', description='Task is being worked on'),
+                Status(name='Completed', description='Task is finished'),
+            ])
